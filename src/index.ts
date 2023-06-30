d7d5eeb5203b6faccd6e1e92091aa822da89c2bd
Beni <black@BeniaminAV>
2023-06-30 11:58:41 +0300

    first commit

---
+++ src/index.ts
@@ -0,0 +1,37 @@
+import express from "express"
+import http from "http"
+import cors from "cors"
+import compression from "compression"
+import bodyParser from "body-parser"
+import cookieParser from "cookie-parser"
+import mongoose from "mongoose"
+import router from "./router"
+
+const app = express()
+
+app.use(express.json())
+
+app.use(cors({ credentials: true }))
+
+app.use(compression())
+app.use(cookieParser())
+app.use(bodyParser.json())
+
+app.get("/", (req, res) => {
+  res.send("Hello Server")
+})
+
+const server = http.createServer(app)
+
+server.listen(5000, () =>
+  console.log("Server is running on port http://localhost:5000")
+)
+
+const MONGO_URL =
+  "mongodb+srv://blackben5:I7otCq38FeM7TPbT@cluster0.0swtxmt.mongodb.net/user"
+
+mongoose.Promise = Promise
+mongoose.connect(MONGO_URL)
+mongoose.connection.on("error", (error: Error) => console.log(error))
+
+app.use("/", router())
